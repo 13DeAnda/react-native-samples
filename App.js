a6c1b2b62b26a33f375deef7eb6c9d875bdfbf7f
13DeAnda <nora.e.deanda@gmail.com>
2020-03-05 19:07:35 -0600

    name guessing

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { StyleSheet, View, Text} from 'react-native';
-import ToDoList from "./components/toDoList/ToDoList";
+import React, {useState} from 'react';
+import { StyleSheet, View} from 'react-native';
+import Header from "./components/Header/Header";
+import StartGameScreen from "./components/screens/startGameScreen";
+import GameScreen from "./components/screens/GameScreen";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
 
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler}/>;
+  if(userNumber){
+    content = <GameScreen userChoice={userNumber}/>;
+  }
   return (
-    <View style ={styles.screen} >
-      <Text> TODO LIST </Text>
-      <ToDoList/>
+    <View style={styles.screen} >
+      <Header title={"Guess a Number"}/>
+      {content}
     </View>
-  );
-}
+  );}
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50
+    flex: 1
   }
 });
